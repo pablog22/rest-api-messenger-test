caccc087e91eed7dbc95e3ae3bb31fb9a32cbaab
Pablo Grasso <pgrasso22@gmail.com>
2016-09-02 11:33:47 -0300

    Add PUT and DELETE

--- src/main/java/ar/com/pg22/test/messenger/resources/MessageResource.java
+++ src/main/java/ar/com/pg22/test/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package ar.com.pg22.test.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -29,6 +31,22 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message){
 		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message){
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateMessage(@PathParam("messageId") long id){
+		messageService.removeMessage(id);
 	}
 	
 	@GET
