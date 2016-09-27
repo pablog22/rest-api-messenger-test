1e94e8e0a698ca132f4810975bf353fea79054a9
Pablo Grasso <pgrasso22@gmail.com>
2016-09-27 00:08:46 -0300

    Create ProfileDao and ProfileRepository

--- src/main/java/ar/com/pg22/test/messenger/service/ProfileService.java
+++ src/main/java/ar/com/pg22/test/messenger/service/ProfileService.java
@@ -1,49 +1,48 @@
 package ar.com.pg22.test.messenger.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import ar.com.pg22.test.messenger.database.DatabaseClass;
+import ar.com.pg22.test.messenger.database.ProfileDao;
 import ar.com.pg22.test.messenger.model.Profile;
 
 public class ProfileService {
 	
 	final static Logger logger = LogManager.getLogger(ProfileService.class);
-
-	private Map<String, Profile> profiles;
 	
-	public ProfileService(DatabaseClass database) {
+	@Autowired
+	private ProfileDao profileDao;
+	
+	public ProfileService() {
 		logger.debug("Initialising ProfileService - Start.");
-		this.profiles = database.getProfiles();
-		
-		if (profiles.isEmpty()) {
-			logger.debug("Adding dummy profiles.");
-			profiles.put("koushik", new Profile(1L, "koushik", "Koushik", "Kothagal"));
-		}
+//		this.profiles = database.getProfiles();
+//		if (profiles.isEmpty()) {
+//			logger.debug("Adding dummy profiles.");
+//			profiles.put("koushik", new Profile(1L, "koushik", "Koushik", "Kothagal"));
+//		}
 		logger.debug("Initialising ProfileService - End.");
 	}
 	
 	public List<Profile> getAllProfiles() {
 		logger.debug("Getting all profiles.");
-		return new ArrayList<Profile>(profiles.values()); 
+		return profileDao.getAllProfiles();
 	}
 	
 	public Profile getProfile(String profileName) {
 		logger.debug("Geting profile with name {}.", profileName);
-		return profiles.get(profileName);
+		return profileDao.getProfile(profileName);
 	}
 	
 	public Profile addProfile(Profile profile) {
 		logger.debug("Adding new profile with name {}.", profile.getProfileName());
-		profile.setId(profiles.size() + 1);
-		logger.debug("New message id is {}", profile.getId());
-		profiles.put(profile.getProfileName(), profile);
+//		profile.setId(profiles.size() + 1);
+//		logger.debug("New message id is {}", profile.getId());
+		Profile newProfile = profileDao.saveProfile(profile);
 		logger.debug("Profile addition finished.");
-		return profile;
+		return newProfile;
 	}
 	
 	public Profile updateProfile(Profile profile) {
@@ -51,12 +50,15 @@ public class ProfileService {
 		if (profile.getProfileName().isEmpty()) {
 			return null;
 		}
-		profiles.put(profile.getProfileName(), profile);
-		return profile;
+		// TODO This is an upsert, it does not check if the object already exists
+		Profile updatedProfile = profileDao.saveProfile(profile);
+		return updatedProfile;
 	}
 	
 	public Profile removeProfile(String profileName) {
 		logger.debug("Removing profile with name {}.", profileName);
-		return profiles.remove(profileName);
+		Profile oldProfile = profileDao.getProfile(profileName);
+		profileDao.removeProfile(profileName);
+		return oldProfile;
 	}
 }
